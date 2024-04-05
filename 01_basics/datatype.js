2579338b92e754bcd4fe57cd2e77c20ee17619fc
Arkopravo Bhuin <99447101+Arkopravo1995@users.noreply.github.com>
2024-04-05 06:44:33 +0000

    datatype added

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+"use strict" // treat all js code as a newer version of js
+
+//number
+//bigint
+//null == > standalone / object
+//string = " ";
+let a ="arkopravo";
+//boolean == >
+//symbol ==>unique
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof a);
