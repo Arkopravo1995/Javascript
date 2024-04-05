aaad00eb6efde23533318bd3f88280dab6907b0f
Arkopravo Bhuin <99447101+Arkopravo1995@users.noreply.github.com>
2024-04-05 05:10:17 +0000

    variable done

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const acId = 2030103;
+let email = "arka.google@com";
+var password = "302901";
+city ="KOlkata";
+let acstate;
+//acId = 30;
+email ="apb@hotmail.com";
+password= "3902i0";
+city ="bengaluru";
+/*
+do not use var because the issue of block scope and functional scope;
+*/
+console.table([acId,email,password,city,acstate]);
